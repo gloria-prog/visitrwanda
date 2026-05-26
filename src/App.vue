c2a03a19eefaa2796c27ba132fa0e80d5692b8b9
muhiza09 <muhizaamina@gmail.com>
2026-05-26 09:49:29 +0200

    my first commit on my project

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+import logo from './assets/sos.png'
+</script>
+<template>
+ <img :src="logo" alt="SOS Logo" class="w-70"/>
+  <h1 class="font-bold text-center text-blue-500"> SOS BLOG</h1>
+  <p class="italic  text-green-600">
+   About SOS Technical High School Kinyinya<br/> <br/>
+SOS Technical High School Kinyinya is a well known technical and vocational school located in Kinyinya, Kigali, Rwanda. The school provides quality education that combines academic learning with practical technical skills. Its main goal is to prepare students for higher education, employment, and success in the modern world of technology and innovation. The school also promotes discipline, responsibility, teamwork, and creativity among learners.
+</br> </br>
+Description<br/> <br/>
+The school offers a supportive learning environment where students are encouraged to develop their talents and gain real world experience through practical learning. SOS Technical High School focuses on helping students become confident and skilled individuals who can contribute positively to society. With qualified teachers and modern learning approaches, the school continues to support students in both personal and professional growth.
+<br/> <br/>
+Departments<br/> <br/>
+SOS Technical High School has different departments and technical programs that help students specialize in various career fields, including:
+<br/> <br/>
+Software Development<br/> <br/>
+Computer Networking<br/> <br/>
+Electronics<br/> <br/>
+General Studies<br/> <br/>
+Technical and Vocational Training<br/> <br/>
+These departments provide students with both theoretical knowledge and hands on experience to prepare them for future opportunities in technology and other professional careers. 
+  </p>
+</template>
+<style scoped></style>
