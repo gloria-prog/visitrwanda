32dcf99f0e0664eb3b7d27d103e1c51e36225651
muhiza09 <muhizaamina@gmail.com>
2026-05-27 08:12:16 +0200

    navbar added

---
+++ src/components/navbar.vue
@@ -0,0 +1,29 @@
+<script setup>
+import logo from '../assets/logo.jpg'
+</script>
+<template>
+<div class="bg-blue-500 text-white p-4">
+  <div class="flex items-center gap-2">
+    <img 
+      :src="logo" 
+      alt="logo"
+      class="w-10 h-10 rounded-full"
+    >
+    <h1 class="font-bold">
+      Tembera Urwanda
+    </h1>
+  </div>
+  <div class="flex gap-4 mt-3">
+    <a href="#">Home</a>
+    <a href="#">About</a>
+    <details>
+      <summary>Media</summary>
+      <div class="ml-3">
+        <p>Images</p>
+        <p>Videos</p>
+      </div>
+    </details>
+    <a href="#">Contact</a>
+  </div>
+</div>
+</template>
---
+++ src/views/home.vue
@@ -0,0 +1,4 @@
+<script setup></script>
+ <template>
+<h1 class="text-center  font-bold text-blue-500">TEMBERA URWANDA PROJECT</h1>
+</template>
